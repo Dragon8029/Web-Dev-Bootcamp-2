ab0152d23163dcb4de8a1e6406d3f5a0ff90143f
Cam Utz <Dragon8029@gmail.com>
2019-03-22 11:22:32 -0700

    add the list of campgrounds

--- YelpCamp/v1/app.js
+++ YelpCamp/v1/app.js
@@ -15,7 +15,7 @@ app.get("/campgrounds", function(req, res) {
         {name: "Mountain Goat's Rest", image: "https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60"},
     ]
 
-    res.render("campgrounds");
+    res.render("campgrounds", {campgrounds:campgrounds});
 });
 
 app.listen(port, () => console.log(`Yelp Camp Server is Connected on port: ${port}!`))
