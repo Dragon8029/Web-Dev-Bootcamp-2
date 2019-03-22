34dc956e4fc1b162a193a0c0f0366d39526efdfd
Cam Utz <Dragon8029@gmail.com>
2019-03-22 11:11:15 -0700

    add campgrounds route

--- YelpCamp/v1/app.js
+++ YelpCamp/v1/app.js
@@ -8,4 +8,14 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+app.get("/campgrounds", function(req, res) {
+    var campgrounds = [
+        {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60"},
+        {name: "Granite Hill", image: "https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60"},
+        {name: "Mountain Goat's Rest", image: "https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60"},
+    ]
+
+    res.render("campgrounds");
+});
+
 app.listen(port, () => console.log(`Yelp Camp Server is Connected on port: ${port}!`))
