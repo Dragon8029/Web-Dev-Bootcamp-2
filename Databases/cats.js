0cc758071adcfc85b0333bdc5a8e90a96ba71874
Cam Utz <Dragon8029@gmail.com>
2019-03-24 16:58:35 -0700

    add Cat.find() so we can see all the cats in the database

--- Databases/cats.js
+++ Databases/cats.js
@@ -9,17 +9,27 @@ var catSchema = new mongoose.Schema({
 
 var Cat = mongoose.model("Cat", catSchema);
 
-var george = new Cat({
-    name: "Mrs. Norris",
-    age: 7, 
-    temperament: "Evil"
-});
+// var george = new Cat({
+//     name: "Mrs. Norris",
+//     age: 7, 
+//     temperament: "Evil"
+// });
 
-george.save(function(err, cat){
+// george.save(function(err, cat){
+//     if(err){
+//         console.log("SOMETHING WENT WRONG!")
+//     } else {
+//         console.log("WE JUST SAVED A CAT TO THE DB:")
+//         console.log(cat);
+//     }
+// });
+
+Cat.find({}, function(err, cats){
     if(err){
-        console.log("SOMETHING WENT WRONG!")
+        console.log("OH NO, ERROR!");
+        console.log(err);
     } else {
-        console.log("WE JUST SAVED A CAT TO THE DB:")
-        console.log(cat);
+        console.log("ALL THE CATS.....");
+        console.log(cats);
     }
 });
