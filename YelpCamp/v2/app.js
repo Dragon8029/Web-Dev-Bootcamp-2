050f604738086d76d57c1fd7280070340bc43602
Cam Utz <Dragon8029@gmail.com>
2019-03-24 17:07:36 -0700

    initial commit for v2

---
+++ YelpCamp/v2/app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 3000
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+var campgrounds = [
+    {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60"},
+    {name: "Granite Hill", image: "https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60"},
+    {name: "Mountain Goat's Rest", image: "https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60"},
+]
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res) {
+    res.render("campgrounds", {campgrounds:campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    // get data form form and add to campgrounds array
+    // redirect back to campgrounds page
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new.ejs");
+});
+
+app.listen(port, () => console.log(`Yelp Camp Server is Connected on port: ${port}!`))
