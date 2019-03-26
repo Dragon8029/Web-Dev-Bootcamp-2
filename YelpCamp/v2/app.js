25d2f798253301e3f2a7edbac99755908c178edd
Cam Utz <Dragon8029@gmail.com>
2019-03-25 18:42:29 -0700

    fix Url Parser error

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -4,9 +4,10 @@ const port = 3000
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
 
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
@@ -16,8 +17,8 @@ var campgroundSchema = new mongoose.Schema({
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 Campground.create(
-    {name: "Salmon Creek", 
-     image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60"}, function(err, campground){
+    {name: "Granite Hill", image: "https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60"
+    }, function(err, campground){
         if(err) {
             console.log(err);
         } else {
