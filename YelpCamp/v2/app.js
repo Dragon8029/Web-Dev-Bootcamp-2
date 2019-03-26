7a7ac755ea6b8174d0aa1b9c906f8b9a449cb35c
Cam Utz <Dragon8029@gmail.com>
2019-03-26 16:57:25 -0700

    add show.ejs and update app.js

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -17,17 +17,17 @@ var campgroundSchema = new mongoose.Schema({
 });
 var Campground = mongoose.model("Campground", campgroundSchema);
 
-Campground.create(
-    {name: "Granite Hill", image: "https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
-    description: "This is a huge granite hill, no bathrooms, No water. Beautiful granite!"
-    }, function(err, campground){
-        if(err) {
-            console.log(err);
-        } else {
-            console.log("NEWLY CREATED CAMPGROUND: ");
-            console.log(campground);
-        }
-     });
+// Campground.create(
+//     {name: "Granite Hill", image: "https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
+//     description: "This is a huge granite hill, no bathrooms, No water. Beautiful granite!"
+//     }, function(err, campground){
+//         if(err) {
+//             console.log(err);
+//         } else {
+//             console.log("NEWLY CREATED CAMPGROUND: ");
+//             console.log(campground);
+//         }
+//      });
 
 
 
@@ -69,10 +69,11 @@ app.get("/campgrounds/new", function(req, res){
     res.render("new.ejs");
 });
 
+// SHOW - shows more info about one campground
 app.get("/campgrounds/:id", function(req, res){
     //find the campground with provided ID
-    //
-    res.send("THIS WILL BE THE SHOW PAGE ONE DAY!");
+    // render show template with that campground
+    res.render("show");
 });
 
 app.listen(port, () => console.log(`Yelp Camp Server is Connected on port: ${port}!`))
