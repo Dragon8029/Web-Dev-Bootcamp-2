abf8f9f2e97cbad1a4da59e9e2f00ff55d506351
Cam Utz <Dragon8029@gmail.com>
2019-03-27 18:38:48 -0700

    add models/comment.js, and proper paths in app.js and models/campground.js

--- YelpCamp/v3/app.js
+++ YelpCamp/v3/app.js
@@ -3,8 +3,10 @@ const express    = require('express'),
       port       = 3000,
       bodyParser = require("body-parser"),
       mongoose   = require("mongoose"),
-      Campground = require("./models/campground");
+      Campground = require("./models/campground"),
+      seedDB     = require("./seeds");
       
+seedDB();
 
 mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
