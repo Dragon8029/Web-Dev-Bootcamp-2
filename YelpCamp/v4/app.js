811db78dd25c28e044ed5ffdc46783cd2263ebb1
Cam Utz <Dragon8029@gmail.com>
2019-03-31 06:55:39 -0700

    add the new comments route

--- YelpCamp/v4/app.js
+++ YelpCamp/v4/app.js
@@ -4,6 +4,7 @@ const express    = require('express'),
       bodyParser = require("body-parser"),
       mongoose   = require("mongoose"),
       Campground = require("./models/campground"),
+      Comment    = require("./models/comment"),
       seedDB     = require("./seeds");
       
 seedDB();
@@ -82,6 +83,29 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
         }
     })
     
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    // lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        }else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err) {
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect('/campgrounds/' + campground._id);
+                }
+            })
+        }
+    })
+    // create new comment
+    // connect new comment to campground
+    // redirect campground show page
 });
 
 app.listen(port, () => console.log(`Yelp Camp Server is Connected on port: ${port}!`))
