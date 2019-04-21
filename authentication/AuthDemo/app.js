786390690fb7a0c9401cc5ba7d3a3f62178ced1f
Cam Utz <Dragon8029@gmail.com>
2019-04-21 08:13:57 -0700

    add required packages

--- authentication/AuthDemo/app.js
+++ authentication/AuthDemo/app.js
@@ -1,10 +1,22 @@
-var express = require("express");
+var express               = require("express"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"), 
+    bodyParser            = require("body-parser"), 
+    LocalStrategy         = require("passport-local"), 
+    passportLocalMongoose = require("passport-local-mongoose")
+mongoose.connect("mongodb://localhost/auth_demo_app");
+
 
 var app = express();
-app.set('view engine', ejs);
+app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
     res.render("home");
-})
+});
 
-app.listen(port, () => console.log(`Server is started on port: ${port}!`))
+app.get("/secret", function(req, res){
+    res.render("secret");
+});
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log('server started.....');
+})
