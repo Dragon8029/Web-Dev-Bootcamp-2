a4e7666cc470502a0bd48586c2e1988d636ec43b
Cam Utz <Dragon8029@gmail.com>
2019-04-22 19:06:31 -0700

    bug fix

--- authentication/AuthDemo/app.js
+++ authentication/AuthDemo/app.js
@@ -1,7 +1,8 @@
 var express               = require("express"),
     mongoose              = require("mongoose"),
     passport              = require("passport"), 
-    bodyParser            = require("body-parser"), 
+    bodyParser            = require("body-parser"),
+    User                  = require("./models/user"), 
     LocalStrategy         = require("passport-local"), 
     passportLocalMongoose = require("passport-local-mongoose")
 mongoose.connect("mongodb://localhost/auth_demo_app", { useNewUrlParser: true });
@@ -12,14 +13,19 @@ const PORT = process.env.PORT || 3001;
 
 var app = express();
 app.set('view engine', 'ejs');
-app.use(passport.initialize());
-app.use(passport.session());
+
+
 
 app.use(require("express-session")({
     secret: "Rusty is the best and cutest dog in the world", 
     resave: false,
     saveUninitialized: false
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("home");
